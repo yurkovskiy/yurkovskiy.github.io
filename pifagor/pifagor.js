41c923b9e1cd1a8456803de2b74d4b74ca3df82c
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2020-03-27 19:02:49 +0200

    dom games

---
+++ pifagor/pifagor.js
@@ -0,0 +1,14 @@
+var main = document.getElementById("main");
+
+// Pifagor's table generator
+for (var i = 1; i <= 9; i++) {
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (var j = 1; j <= 9; j++) {
+        var cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.innerText = i * j;
+        row.appendChild(cell);
+    }
+    main.appendChild(row);
+}
