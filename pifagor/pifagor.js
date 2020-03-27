eb1706f2d726463aa9621996ef6bd67e419a8564
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2020-03-27 19:09:28 +0200

    new css for pifagor

--- pifagor/pifagor.js
+++ pifagor/pifagor.js
@@ -4,9 +4,9 @@ var main = document.getElementById("main");
 for (var i = 1; i <= 9; i++) {
     var row = document.createElement("div");
     row.setAttribute("class", "row");
-    for (var j = 1; j <= 9; j++) {
+    for (var j = 1; j <= 9; j++) {  
         var cell = document.createElement("div");
-        cell.setAttribute("class", "cell");
+        cell.setAttribute("class", "cell cell-main");
         cell.innerText = i * j;
         row.appendChild(cell);
     }
