ec0fd5d3ff026e348ecb48820b2ed1bee381376b
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2026-08-10 10:15:39 +0300

    [ADD] session 03

---
+++ Frontend-Fundamentals/data_03.js
@@ -0,0 +1,44 @@
+// data.js
+export const checklistData = [
+    { 
+        id: 1, 
+        task: "0. Fast Review Previous Session", 
+        completed: false,
+        subitems: [
+            { id: 101, task: "HTML Skeleton and Elements", completed: false },
+            { id: 102, task: "HEAD / BODY", completed: false },
+            { id: 103, task: "Attributes of HTML elements", completed: false },
+        ]
+    },
+    { 
+        id: 2, 
+        task: "1. Cascading Style Sheets",
+        completed: false,
+        subitems: [
+            { id: 201, task: "CSS Purpose", completed: false },
+            { id: 202, task: "CSS Inheritance: Browser default styles", completed: false },
+        ]
+    },
+    { 
+        id: 3, 
+        task: "2. CSS Selectors",
+        completed: false,
+        subitems: [
+            { id: 301, task: "Basic Selectors: Tag, Class, ID", completed: false },
+            { id: 302, task: "The Weight (Specificity) of Selectors", completed: false },
+            { id: 303, task: "Pseudo-classes", completed: false },
+            { id: 304, task: "Pseudo-elements", completed: false },
+        ]
+    },
+    { 
+        id: 4, 
+        task: "3. CSS BOX Model",
+        completed: false,
+        subitems: [
+            { id: 401, task: "Box model purpose. CSS Properties", completed: false },
+            { id: 402, task: "margin", completed: false },
+            { id: 403, task: "padding", completed: false },
+            { id: 404, task: "border", completed: false },
+        ]
+    },
+];
