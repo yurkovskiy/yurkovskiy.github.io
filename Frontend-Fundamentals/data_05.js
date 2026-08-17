1054b0f795227cec4833f22d3579f04b2e2a978c
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2026-08-17 23:34:00 +0300

    [ADD] Session 05

--- Frontend-Fundamentals/data_05.js
+++ Frontend-Fundamentals/data_05.js
@@ -24,5 +24,15 @@ export const checklistData = [
             { id: 303, task: "properties: flex-wrap, justify-content, align-self, order", completed: false },
             { id: 304, task: "properties: flex-grow, flex-shrink, flex-direction, gap/flex-gap", completed: false }
         ]
-    }
+    },
+    { 
+        id: 4, 
+        task: "3. CSS Libraries / Frameworks",
+        completed: false,
+        subitems: [
+            { id: 401, task: "Bootstrap", completed: false },
+            { id: 402, task: "Tailwind CSS", completed: false },
+            { id: 403, task: "Bulma / Foundation, .....", completed: false },
+        ]
+    },
 ];
