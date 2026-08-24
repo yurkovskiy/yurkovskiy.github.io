24436fe44bc1c191a44ee1cd741a07ca484b402c
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2026-08-24 06:07:31 +0300

    [UPDATE] session 07 JS

--- Frontend-Fundamentals/data_07.js
+++ Frontend-Fundamentals/data_07.js
@@ -7,10 +7,35 @@ export const checklistData = [
         subitems: [
             { id: 101, task: "Little History", completed: false },
             { id: 102, task: "What can you do with JS in Browser? [Live Example]", completed: false },
-            { id: 103, task: "Rules: Keywords, Syntax", completed: false },
-            { id: 104, task: "Variables", completed: false },
-            { id: 105, task: "Scope: block, local, global", completed: false },
-            { id: 106, task: "Where can you run js code?", completed: false },
+            { id: 103, task: "Where can you run js code?", completed: false },
+            { id: 104, task: "ECMA-Script Standard: ES5, ES2015+, ...", completed: false },
+            { id: 105, task: "Rules: Keywords, Syntax", completed: false },
+            { id: 106, task: "Variables", completed: false },
+            { id: 107, task: "Scope: block, local, global / var, let, const", completed: false },
+            { id: 108, task: "Datatypes", completed: false },
+            { id: 109, task: "Template Strings", completed: false }
+        ]
+    },
+    { 
+        id: 2, 
+        task: "1. JS Control Statements", 
+        completed: false,
+        subitems: [
+            { id: 201, task: "Alternative: if, if-else, switch statement", completed: false },
+            { id: 202, task: "Cycles: for, while, do-while", completed: false },
+            { id: 203, task: "Cycles: break, continue", completed: false }
+        ]
+    },
+    { 
+        id: 3, 
+        task: "2. Functions",
+        completed: false,
+        subitems: [
+            { id: 301, task: "Functions. Purpose", completed: false },
+            { id: 302, task: "Function declaration", completed: false },
+            { id: 303, task: "Hoisting", completed: false },
+            { id: 304, task: "Function expression", completed: false },
+            { id: 305, task: "Arguments. Callbacks", completed: false },
         ]
     }
 ];
